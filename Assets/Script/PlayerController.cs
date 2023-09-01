ba9532ad80733e00649c122bada05e2d9aae6294
Westherito <raphaelxs100@gmail.com>
2023-08-31 23:03:14 -0300

    Posição dos tiros e interações com player

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,14 +7,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D rbPlayer;
     private Vector2 movPlayer;
     [SerializeField] private float vel;
+    [SerializeField] private GameObject tiros;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovimentoPlayer();
+    }
+
+    private void MovimentoPlayer()
     {
         // Pegando o Input do usuário e adicionando velocidade
         movPlayer.x = Input.GetAxis("Horizontal");
@@ -23,5 +29,14 @@ public class PlayerController : MonoBehaviour
         movPlayer.Normalize();
         // Passando para o player
         rbPlayer.velocity = movPlayer * vel;
+        TirosPlayer();
+    }
+
+    private void TirosPlayer()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Instantiate(tiros, transform.position, transform.rotation);
+        }
     }
 }
