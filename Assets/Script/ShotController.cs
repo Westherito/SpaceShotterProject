f07de62ae749a3cc82e754f87435973f3508f8fe
Westherito <raphaelxs100@gmail.com>
2023-08-31 22:36:00 -0300

    Criando prefabs e tiros
    Adicionando prefabs ao player e aos tiros e criando uma lógica para mover os tiros para cima

---
+++ Assets/Script/ShotController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rbObj;
+    [SerializeField] private float vel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Movimento do tiro para cima
+        rbObj.velocity = Vector2.up * vel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
