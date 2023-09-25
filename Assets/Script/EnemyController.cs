37da674eb8a1beb283410b325a03de83f840387c
Westherito <raphaelxs100@gmail.com>
2023-09-25 18:59:56 -0300

    Criando tiros dos inimigos

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -7,6 +7,10 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
 
     [SerializeField] private float vel;
+
+    [SerializeField]private GameObject enemyBullet;
+
+    private float timerBullet = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +20,15 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         //Alterando a velocidade 
         rb.velocity = new Vector2 (0f, vel);
+        //instanciando tiros com delay
+        timerBullet -= Time.deltaTime;
+        if (timerBullet < 0f){
+            Instantiate(enemyBullet, transform.position, transform.rotation);
+
+            timerBullet = 1f;
+        }
     }
 }
