b285794ce0e49b87cdc8ce0b3f6207e67482afe4
Westherito <raphaelxs100@gmail.com>
2023-10-01 11:59:33 -0300

    Animando e randomizado tiros

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -8,27 +8,33 @@ public class EnemyController : MonoBehaviour
 
     [SerializeField] private float vel;
 
-    [SerializeField]private GameObject enemyBullet;
+    [SerializeField] private GameObject enemyBullet;
 
     private float timerBullet = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timerBullet = Random.Range(0.5f, 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Verificando se a sprite está visivel na tela
+        bool visible = GetComponentInChildren<SpriteRenderer>().isVisible;
 
-        //Alterando a velocidade 
-        rb.velocity = new Vector2 (0f, vel);
-        //instanciando tiros com delay
-        timerBullet -= Time.deltaTime;
-        if (timerBullet < 0f){
-            Instantiate(enemyBullet, transform.position, transform.rotation);
+        if (visible)
+        {
+            //Alterando a velocidade 
+            rb.velocity = new Vector2(0f, vel);
+            //instanciando tiros com delay
+            timerBullet -= Time.deltaTime;
+            if (timerBullet < 0f)
+            {
+                Instantiate(enemyBullet, transform.position, transform.rotation);
 
-            timerBullet = 1f;
+                timerBullet = Random.Range(1.5f, 2f);
+            }
         }
     }
 }
