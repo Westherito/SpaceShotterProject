e1c0bea1093149a8dbf0122efe5108020181d1a4
Westherito <raphaelxs100@gmail.com>
2023-11-14 21:15:52 -0300

    Fazendo o inimigo 2

---
+++ Assets/Script/EnemyController02.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController02 : EnemyFatherController
+{
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private GameObject enemyBullet;
+
+    private float timerBullet = 1f;
+
+    [SerializeField] private Transform posTiro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerBullet = Random.Range(0.5f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyTiros();
+    }
+
+    private void EnemyTiros()
+    {
+        // Verificando se a sprite está visivel na tela
+        bool visible = GetComponentInChildren<SpriteRenderer>().isVisible;
+
+        if (visible)
+        {
+            //Alterando a velocidade 
+            rb.velocity = new Vector2(0f, vel);
+            //instanciando tiros com delay
+            timerBullet -= Time.deltaTime;
+            if (timerBullet < 0f)
+            {
+                Instantiate(enemyBullet, transform.position, transform.rotation);
+
+                timerBullet = Random.Range(1.5f, 2f);
+            }
+        }
+    }
+}
+    
+
