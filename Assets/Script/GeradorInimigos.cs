8f7c6e7c41932b1fbed7a3b5428684886a3cec2a
Westherito <raphaelxs100@gmail.com>
2023-11-20 14:09:36 -0300

    Fazendo o gerador de inimigos

---
+++ Assets/Script/GeradorInimigos.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorInimigos : MonoBehaviour
+{
+    [SerializeField] private GameObject[] inimigos;
+    private int pontos = 0;
+    [SerializeField] private int level = 1;
+    private float timer = 0f;
+    [SerializeField] private float intervaloTimer = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GeraInimigo();
+    }
+
+    private void GeraInimigo()
+    {
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (timer <= 0f)
+        {
+            int numInimigos = level * 4;
+            int qteInimigos = 0;
+            while (numInimigos > qteInimigos)
+            {
+                GameObject inimigoCriado;
+
+                float chance = Random.Range(0, level);
+                if (chance > 2f)
+                {
+                    inimigoCriado = inimigos[1];
+                }
+                else
+                {
+                    inimigoCriado = inimigos[0];
+                }
+
+                Vector3 pos = new Vector3(Random.Range(-8f, 8f), Random.Range(6f, 17f), 0f);
+
+                Instantiate(inimigoCriado, pos, transform.rotation);
+
+                qteInimigos++;
+
+                timer = intervaloTimer;
+            }
+        }
+    }
+}
