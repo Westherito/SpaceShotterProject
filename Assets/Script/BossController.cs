20a91a9e1a3c3d74564caedbe03abb87b766d97a
Westherito <raphaelxs100@gmail.com>
2023-12-05 16:20:48 -0300

    iniciando a maquina de estados do boss

--- Assets/Script/BossController.cs
+++ Assets/Script/BossController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
+    [SerializeField] private string estado = "estado1";
+    [SerializeField] private float vel = 2f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Collider2D col;
+    private float limiteX, limiteY;
+    private bool dir = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,52 @@ public class BossController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (estado) 
+        {
+            case "estado1" :
+                Estado1();
+                break;
+            case "estado2":
+                Estado2();
+                break;
+            case "estado3":
+                Estado3();
+                break;
+        }
+    }
+
+    private void Estado1()
+    {
         
+        // mudando a direção do boss direita e esquerda
+        limiteX = 7.342f;
+        if (dir == true)
+        {
+            rb.velocity = new Vector2(vel, 0f);
+
+        }
+        else
+        {
+            rb.velocity = new Vector2(-vel, 0f);
+
+        }
+        // var de controle para o boss
+        if (transform.position.x >= limiteX)
+        {
+            dir = false;
+        }
+        if (transform.position.x <= -limiteX)
+        {
+            dir = true;
+        }
+
+    }
+    private void Estado2()
+    {
+
+    }
+    private void Estado3()
+    {
+
     }
 }
